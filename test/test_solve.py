36ddcfcdd438ff883395e16e175a502b3d6c48b7
Arash Rouhani <miffoljud@gmail.com>
2012-10-01 13:16:17 -0400

    Fix naming of tests

--- test/test_solve.py
+++ test/test_solve.py
@@ -3,14 +3,14 @@ from src.solve import *
 import src
 
 class TestSolveFunctions(unittest.TestCase):
-  def test_get_filters(self):
+  def test_get_filterss(self):
     src.solve.fetch_pools = lambda g, d: g+"o"+d
     src.solve.std_directions = lambda: ["a", "b"]
     got = src.solve.get_filterss("k")
     exp = {"a": list("koa"), "b": list("kob")}
     self.assertEqual(got, exp)
 
-  def test_get_new_figure_pair(self):
+  def test_get_from_figure(self):
     grid = [["a", "b"],
             ["c"]]
     self.assertEqual(get_from_figure(grid, (1, 1)), "a")
