fd23129255bd54acf8076fa3349dc5fe6ee317d5
Arash Rouhani <miffoljud@gmail.com>
2012-10-01 11:19:00 -0400

    Some inference intelligence for modifications

--- src/pool/modifiers.py
+++ src/pool/modifiers.py
@@ -1,3 +1,6 @@
+from copy import copy
+from src.pool.helpers import concat_map
+
 class Modifier:
   def __init__(self, modify, punishment, message):
     self.modify = modify
@@ -5,5 +8,29 @@ class Modifier:
     self.message = message
 
 def infer_modifiers(figure_pair):
-  return [Modifier(lambda x: x, 10, "no modification")]
+  fig_A, fig_B = figure_pair
+  yield Modifier(lambda x: x, 10, "no modification")
+  all_keys = frozenset(concat_map(lambda x: x.keys(), fig_A+fig_B))
+  for key in all_keys:
+    # Becoming capability
+    for subfig_B in fig_B:
+      value_B = subfig_B.get(key)
+      modifier = map_fig(lambda _: value_B, key)
+      yield Modifier(modifier, 10, "turn %s-values to %s" % (key, value_B))
+
+      # Additive capability
+      for subfig_A in fig_A:
+        value_A = subfig_A.get(key)
+        if type(value_A) == type(value_B) and "__sub__" in dir(value_A):
+          delta = (value_B-value_A)
+          modifier = map_fig(lambda v: v + delta, key)
+          yield Modifier(modifier, 10, "inc %s-values by %s" % (key, delta))
+
+
+def map_fig(modifier, key):
+  def mod_subfig(subfig):
+    subfig = copy(subfig)
+    subfig.update([(key, modifier(subfig.get(key)))])
+    return subfig
+  return mod_subfig
 
--- test/pool/test_modifiers.py
+++ test/pool/test_modifiers.py
@@ -3,14 +3,31 @@ from src.pool.modifiers import *
 import src
 
 def get_all_modifier(*figs):
-  modifiers = infer_modifiers(figs)
+  modifiers = list(infer_modifiers(figs))
   return lambda subfig: map(lambda mod: mod.modify(subfig), modifiers)
 
 class TestSelectorFunctions(unittest.TestCase):
 
   def test_infer_selectors(self):
+    # "Type-checking"
     fig_A = [{}]
     fig_B = [{}]
     go = get_all_modifier(fig_A, fig_B)
-
     self.assertIn({}, go({}))
+
+    # Check simple transforming capability
+    fig_A = [{"value": "white"}]
+    fig_B = [{"value": "black"}]
+    go = get_all_modifier(fig_A, fig_B)
+    self.assertIn({"value": "black"}, go({"value": "white"}))
+    self.assertIn({"value": "black"}, go({"value": "white"}))
+    self.assertIn({"value": "black"}, go({"value": "orange"}))
+
+    # Check subtractive capability
+    fig_A = [{"value": 70}]
+    fig_B = [{"value": 80}]
+    go = get_all_modifier(fig_A, fig_B)
+    self.assertIn({"value": 30}, go({"value": 20}))
+    self.assertNotIn({"value": 31}, go({"value": 20}))
+    self.assertIn({"value": 60}, go({"value": 50}))
+    self.assertIn({"value": 80}, go({"value": 10}))
