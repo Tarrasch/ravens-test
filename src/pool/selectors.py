ce44e4180c4cbbd15b446b4b80c6cd7b6f3f09e1
Arash Rouhani <miffoljud@gmail.com>
2012-10-01 22:41:09 -0400

    Make selectors min-max capable

--- src/pool/selectors.py
+++ src/pool/selectors.py
@@ -1,3 +1,5 @@
+from src.pool.helpers import concat_map
+
 class Selector:
   def __init__(self, selects, punishment, message):
     self.selects = selects
@@ -7,5 +9,17 @@ class Selector:
   def __repr__(self):
     return self.message
 
-def infer_selectors(figure):
-  return [Selector(lambda subfig: True, 0, "All")]
+def infer_selectors(fig):
+  yield Selector(lambda subfig: True, 0, "All")
+  all_keys = frozenset(concat_map(lambda x: x.keys(), fig))
+  for key in all_keys:
+    # Max/min capability
+    values = map(lambda x: x.get(key), fig)
+    if "__cmp__" in dir(values[0]): # Number like
+      for fold, desc in [(min, "min"), (max, "max")]:
+        selector = lambda subfig, v=fold(values), key=key: subfig.get(key) == v
+        yield Selector(
+              selector,
+              10,
+              "%s by %s" % (desc, key))
+
--- test/pool/test_selectors.py
+++ test/pool/test_selectors.py
@@ -1,28 +1,49 @@
 import unittest
 from src.pool.selectors import *
+from itertools import *
 import src
 
-def get_any(fig):
-  selectors = infer_selectors(fig)
-  def find(subfig):
-    return any(map(lambda sel: sel.selects(subfig), selectors))
-def get_anys(fig):
-  selectors = list(infer_selectors(fig))
-  def find(subfigs):
-    return any(map(lambda sel: all(map(lambda subfig:
-      sel.selects(subfig), selectors)), subfigs))
-  return find
+def split_on_condition(seq, condition):
+  # http://stackoverflow.com/a/949490/621449
+  a, b = [], []
+  for item in seq:
+      (a if condition(item) else b).append(item)
+  return a,b
 
-def get_any(fig):
-  find = get_anys(fig)
-  return lambda subfig: find([subfig])
+def get_validator(fig):
+  selectors = list(infer_selectors(fig))
+  def validator(exists, noexists):
+    return any(map(lambda sel:
+      all(map(lambda subfig: sel.selects(subfig), exists))
+      and
+      all(map(lambda subfig: not sel.selects(subfig), noexists))
+      , selectors))
+  return validator
 
 class TestSelectorFunctions(unittest.TestCase):
 
   def test_infer_selectors(self):
     fig = [{}]
-    my_any = get_any(fig)
+    v = get_validator(fig)
 
     # It should always yield one ALL selector
-    self.assertTrue(my_any({}))
-    self.assertTrue(my_any({"irrelevant":"property"}))
+    self.assertTrue(v([{}], []))
+    self.assertTrue(v([{"irrelevant":"property"}], []))
+
+    fig = [{"x": x, "y": y} for x, y in product(range(3),
+      repeat=2)]
+    v = get_validator(fig)
+    self.assertEqual(len(fig), 9) # sanity check
+    self.assertTrue (v(*split_on_condition(fig, lambda sf: sf["x"]==2)))
+    self.assertFalse(v(*split_on_condition(fig, lambda sf: sf["x"]==1)))
+    self.assertTrue (v(*split_on_condition(fig, lambda sf: sf["x"]==0)))
+
+    self.assertTrue (v(*split_on_condition(fig, lambda sf: sf["y"]==2)))
+    self.assertFalse(v(*split_on_condition(fig, lambda sf: sf["y"]==1)))
+    self.assertFalse(v(*split_on_condition(fig, lambda sf: sf["y"]==1)))
+
+    self.assertFalse(v([{"x":2}, {"y":2}], [{"x":1}]))
+    self.assertTrue (v([{"x":2}, {"x":2}], []))
+    self.assertFalse(v([{"x":2}, {"x":1}], [[{"y":1}]]))
+
+
