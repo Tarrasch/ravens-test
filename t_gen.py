044074cd112df1594db0b85b8e8dbd8ba8f17959
Arash Rouhani <miffoljud@gmail.com>
2012-10-11 00:06:43 -0400

    t_gen

--- t_gen.py
+++ t_gen.py
@@ -8,7 +8,7 @@ def make_fig(n):
                 zip(repeat(0), range(-n+1, 0)))
   return [{"x": x, "y": y} for y, x in lists]
 
-grid = [[make_fig(x-y+3) for x in range(3)] for y in range(3)]
+grid = [[make_fig(x-y+4) for x in range(3)] for y in range(3)]
 grid[2].pop()
 
 di= {k: make_fig(k) for k in range(1,6)}
