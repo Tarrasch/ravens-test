d13379797b0ea43d1e6b868712dd31d7559c8a3f
Arash Rouhani <miffoljud@gmail.com>
2012-10-26 20:44:02 -0400

    Yay passes all 6 cases

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.parser import parse
 
 method = solve              # Uncomment to just write the answer number
 # method = solve_verbose      # Uncomment to motivate it's answer
-method = solve_very_verbose # Uncomment to compare motivations between choices
+# method = solve_very_verbose # Uncomment to compare motivations between choices
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
     file_path = "reps/%s" % x
     if os.path.exists(file_path):
       tree = parse(file_path)
-      pprint(tree) # Uncomment to see the intemediete representation
+      # pprint(tree) # Uncomment to see the intemediete representation
       ans = method(tree)
       print "--------------------------------------------------------"
       print "Solution to problem %s is:" % (x)
